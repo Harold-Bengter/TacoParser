481daa1815e70baf4d25db61c58312fd81fae6df
Harold Bengter <Haroldbe55@yahoo.com>
2022-08-07 13:44:53 -0400

    Made changes in prograwm,tacoparcer.cs and tests for more CCR reading

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -12,13 +12,9 @@ namespace LoggingKata
 
         static void Main(string[] args)
         {
-            // TODO:  Find the two Taco Bells that are the furthest from one another.
-            // HINT:  You'll need two nested forloops ---------------------------
 
             logger.LogInfo("Log initialized");
 
-            // use File.ReadAllLines(path) to grab all the lines from your csv file
-            // Log and error if you get 0 lines and a warning if you get 1 line
             var lines = File.ReadAllLines(csvPath);
 
             logger.LogInfo($"Lines: {lines[0]}");
@@ -34,26 +30,16 @@ namespace LoggingKata
                 logger.LogWarning($"Lines: {lines[1]}");
             }
 
-            // Create a new instance of your TacoParser class
             var parser = new TacoParser();
 
-            // Grab an IEnumerable of locations using the Select command: var locations = lines.Select(parser.Parse);
             var locations = lines.Select(parser.Parse).ToArray();
-
-            // DON'T FORGET TO LOG YOUR STEPS
-
-            // Now that your Parse method is completed, START BELOW ----------
-
-            // TODO: Create two `ITrackable` variables with initial values of `null`. These will be used to store your two taco bells that are the farthest from each other.            
+       
             ITrackable TrackA = null;
             ITrackable TrackB = null;
-            // Create a `double` variable to store the distance
+          
             double distance = 0;
+         
 
-            // Include the Geolocation toolbox, so you can compare locations: `using GeoCoordinatePortable;`
-
-            //HINT NESTED LOOPS SECTION---------------------
-            // Do a loop for your locations to grab each location as the origin (perhaps: `locA`)
             foreach (var locA in locations)
             {
                 var corA = new GeoCoordinate(locA.Location.Latitude, locA.Location.Longitude);
